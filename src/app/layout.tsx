2001a8c13c1e71544fc82ab9c628f6c6483c5fcd
Carter Adams <cladams7905@gmail.com>
2024-02-27 08:31:06 +0800

    Add supabase provider to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Analytics } from "@vercel/analytics/react";
 import Navbar from "@components/layout/Navbar";
 //import Footer from "@components/layout/Footer";
+import SupabaseProvider from '@app/supabase/supabase-provider';
 import { Suspense } from "react";
 
 import type { Metadata } from "next";
@@ -20,14 +21,16 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
-        <Suspense fallback="...">
-          <Navbar />
-        </Suspense>
-        <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
-          {children}
-        </main>
-        {/*<Footer />*/}
-        <Analytics />
+        <SupabaseProvider>
+          <Suspense fallback="...">
+            <Navbar />
+          </Suspense>
+          <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
+            {children}
+          </main>
+          {/*<Footer />*/}
+          <Analytics />
+        </SupabaseProvider>
       </body>
     </html>
   );
